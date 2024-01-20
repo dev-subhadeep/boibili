f37fdc843aedd02ee244ec978dd8df310ebbdae4
dev-subhadeep <subhadeepsarkar1995@gmail.com>
2024-01-20 07:53:09 +0530

    refactor: separated database name from database URI

--- server/src/utils/constants.js
+++ server/src/utils/constants.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 
+const dburi = process.env.DB_URI;
+const dbname = process.env.DB_NAME;
 const jwtpk = process.env.JWT_PK;
 
 module.exports = {
   jwtpk,
+  dburi,
+  dbname,
 };
